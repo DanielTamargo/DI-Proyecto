98a1d9baf373ae3be04eb1b0dc5fdd1ffbd87b24
Daniel Tamargo <danieltamargosaiz@gmail.com>
2020-04-26 17:20:54 +0200

    Splash screen + Toolbar

---
+++ Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.proyecto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+
+        handler = Handler()
+        handler.postDelayed({
+            var intento = Intent(this, VentanaPrincipal::class.java)
+            startActivity(intento)
+            finish()
+        }, 100) //3500 milliseconds
+
+    }
+
+    fun init() {
+        progressBar = findViewById(R.id.progressBar)
+    }
+}
---
+++ Proyecto/app/src/main/java/com/example/proyecto/VentanaPrincipal.kt
@@ -0,0 +1,25 @@
+package com.example.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
+class VentanaPrincipal : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana_principal)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        //supportActionBar?.title = "Valorant" //<- no cambiar esto, he añadido un textView en la toolbar para personalizar la fuente
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+}
