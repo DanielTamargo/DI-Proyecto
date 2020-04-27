5f331cd69d2591f50746d65836f64ec90ab72ab1
Daniel Tamargo <danieltamargosaiz@gmail.com>
2020-04-27 14:30:36 +0200

    Click Listener Card View Por fin

--- Proyecto/app/src/main/java/com/example/proyecto/Adaptador.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/Adaptador.kt
@@ -1,17 +1,31 @@
 package com.example.proyecto
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class Adaptador: RecyclerView.Adapter<Adaptador.ViewHolder>() {
 
+    var opciones_main: MutableList<String> = opciones()
+    lateinit var context: Context
 
-    class ViewHolder(v: View):RecyclerView.ViewHolder(v) {
+    private fun opciones(): MutableList<String> {
+        val opciones = mutableListOf<String>()
+        opciones.add("Mapas")
+        opciones.add("Personajes")
+        opciones.add("Armas")
+        return opciones
+    }
+
+    class ViewHolder(v: View):RecyclerView.ViewHolder(v), View.OnClickListener {
         var tv_num: TextView
         var card_view: CardView
         var tv_desc: TextView
@@ -19,7 +33,33 @@ class Adaptador: RecyclerView.Adapter<Adaptador.ViewHolder>() {
             card_view = v.findViewById(R.id.card_view)
             tv_num = v.findViewById(R.id.tv_num)
             tv_desc = v.findViewById(R.id.tv_descripcion)
+            v.setOnClickListener(this)
         }
+
+        override fun onClick(v: View?) {
+            val context = itemView.context
+
+            when (tv_num.text) {
+                "Mapas" -> {
+                    val intent = Intent(context, VentanaMapaHaven::class.java)
+                    context.startActivity(intent)
+                    //Toast.makeText(context, "¡¡Mapas!!", Toast.LENGTH_SHORT).show()
+                }
+                "Personajes" -> {
+                    Toast.makeText(context, "¡¡Personajes!!", Toast.LENGTH_SHORT).show()
+                }
+                "Armas" -> {
+                    Toast.makeText(context, "¡¡Armas!!", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(context, "¡¡Has encontrado el primer Easter Egg!!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            //val intent = Intent(context, VentanaMapaHaven::class.java)
+            //startActivity(intent)
+        }
+
     }
 
     //Situar celda
@@ -31,25 +71,20 @@ class Adaptador: RecyclerView.Adapter<Adaptador.ViewHolder>() {
     //Número de celdas a devolver
     override fun getItemCount(): Int {
         //como la última se ve incompleta si tengo la toolbar, intentaré añadir una y a la útlima le quitaré todos los elementos
-        return 20 + 1
+        return opciones_main.size + 1
     }
 
     //Modificar lo que vaya a salir en la celda
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        
-        //Vaya puta maravilla cómo funcionan las cardviews y las celdas con el wrap content, maravillosisimo
-        holder.tv_desc.visibility = View.GONE
-        if (position % 4 == 0) {
-            holder.tv_desc.visibility = View.VISIBLE
-        }
+
         //La última que sale se buggea, así que creamos una de más pero en vez de darle info, la escondemos
         //Creo que esto puede ocasionar algún bug, pero mejor eso a que la última tarjeta no salga, ¿no?
-        if (position == 20) {
-            holder.card_view.visibility = View.GONE
+        if (position == opciones_main.size) {
+            holder.card_view.visibility = View.INVISIBLE
         } else {
-            if (holder.card_view.visibility == View.GONE) holder.card_view.visibility = View.VISIBLE
-            holder.tv_num.text = "Celda número $position"
+            if (holder.card_view.visibility == View.INVISIBLE) holder.card_view.visibility = View.VISIBLE
+            holder.tv_num.text = opciones_main[position]
         }
     }
 
--- Proyecto/app/src/main/java/com/example/proyecto/VentanaMapaHaven.kt
+++ Proyecto/app/src/main/java/com/example/proyecto/VentanaMapaHaven.kt
@@ -1,32 +1,25 @@
 package com.example.proyecto
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.widget.Toolbar
 
-class VentanaPrincipal : AppCompatActivity() {
+class VentanaMapaHaven : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ventana_principal)
+        setContentView(R.layout.activity_ventana_mapa_haven)
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         //supportActionBar?.title = "Valorant" //<- no cambiar esto, he añadido un textView en la toolbar para personalizar la fuente
 
-        supportFragmentManager.beginTransaction().add(R.id.contenedorCeldas, ContenedorCeldasFragment()).commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
-    }
-
-    fun abrir_ventana_mapas() {
-        val intent = Intent(this, VentanaMapaHaven::class.java)
-        startActivity(intent)
     }
 }
